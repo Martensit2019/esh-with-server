f1fa511c2ed4634bb9451376ff1b8c876f7af87b
Martensit2019 <spichakandrey79@yandex.ru>
2022-12-04 16:01:39 +0300

    add auth

--- server/srartUp/initDatabase.js
+++ server/srartUp/initDatabase.js
@@ -1,7 +1,9 @@
 const Category = require('../models/Category')
 const Product = require('../models/Product')
+const User = require('../models/User')
 const categoryMock = require('../mock/categories.json')
 const productMock = require('../mock/products.json')
+const userMock = require('../mock/users.json')
 
 module.exports = async () => {
   const categories = await Category.find()
@@ -11,6 +13,10 @@ module.exports = async () => {
   const products = await Product.find()
   if (products.length !== productMock.length) {
     await createInitialEntity(Product, productMock)
+  }
+  const users = await User.find()
+  if (users.length !== userMock.length) {
+    await createInitialEntity(User, userMock)
   }
 }
 
